b09f6227585268c85bd2c6337622bed3036faafb
The-Luan Tran <the-luan.tran@mail.mcgill.ca>
2018-02-25 19:19:55 -0500

    implemented fsm for read_controller

--- P3_cache/read_controller.vhd
+++ P3_cache/read_controller.vhd
@@ -15,9 +15,61 @@ entity read_controller is
     );
 end read_controller;
 
-architecture arch of read_controller is
+ARCHITECTURE arch OF read_controller IS
+type  read_states is (I, R, MR, MW, RP, D);  -- Define the states
+signal state : read_states;
 
 BEGIN
 
+read_fsm : process(clock, reset)
+variable var_hit: std_logic := '0';
+variable var_valid: std_logic := '0';
+variable var_dirty: std_logic := '0';
+variable m_waitrequest: std_logic := '0';
+BEGIN
+    if (reset ='1') then
+        state <= I;
+    elsif (rising_edge(clock)) then
+        case state is
+            when I =>
+                if (s_read = '1') then             
+                    state <= R;
+                end if;
+            when R =>
+                s_waitrequest <= '1';
+                if (var_hit = '1') then
+                    if (var_valid = '1') then
+                        state <= D;
+                    end if;
+                else
+                    if (var_valid = '1') then
+                        if (var_dirty = '1') then
+                            state <= MW;
+                        else
+                            state <= MR;
+                        end if;
+                    end if;
+                end if;
+            when D =>
+                s_waitrequest <= '0';
+                state <= I;
+            when MW =>
+                if (m_waitrequest = '1') then
+                    state <= MW;
+                else
+                    state <= MR;
+                end if;
+            when MR =>
+                if (m_waitrequest = '1') then
+                    state <= MR;
+                else
+                    state <= RP;
+                end if;
+            when RP =>
+                state <= D;
+        end case;
+    end if;
+
+end process read_fsm;
 
 end arch;
