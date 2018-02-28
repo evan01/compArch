c4e4e5b51394d8cab176f2c46bb601983ad13bcc
Callum May <callumdmay@gmail.com>
2018-02-27 19:56:18 -0500

    Add basic scaffolding for cache.vhd

--- P3_cache/cache.vhd
+++ P3_cache/cache.vhd
@@ -28,10 +28,77 @@ port(
 end cache;
 
 architecture arch of cache is
+  type  CACHE_STATES is (I, R, W, MR, MW, MWAIT);  -- Define the states
+  signal state : CACHE_STATES := I;
 
+  type cache_array is array(31 downto 0) of std_logic_vector(135 downto 0);
 
 begin
 
--- make circuits here
+cache : process(clock, reset)
+  variable tag: std_logic_vector(5 downto 0); --remaining bits up to 15th
+  variable index: std_logic_vector(4 downto 0); --next 5 bits
+  variable offset: std_logic_vector(1 downto 0); --Last 4 bits of address - 2 last
+  variable cache_block: std_logic_vector(135 downto 0);
+  variable valid : std_logic;
+  variable dirty : std_logic;
+begin
+  if (reset = '1') then
+    state <= I;
+  elsif(rising_edge(clock)) then
+    case state is
+      when I =>
+        s_waitrequest <= '1';
+        if (s_read = '1') then
+          state <= R;
+        elsif (s_write = '1') then
+          state <= W;
+        else
+          state <= I;
+        end if;
+      when R =>
+       offset := s_addr(3 downto 2);
+       index := s_addr(8 downto 4);
+       tag := s_addr(14 downto 9);
+
+       cache_block := cache_array(to_integer(unsigned(index)));
+
+       valid := cache_block(135);
+       dirty := cache_block(134);
+
+       --check validity
+       if(valid = '1') then
+           --check for cache hit
+           if (cache_block(133 downto 128) = tag) then
+               --Hit
+               s_readdata <= cache_block(31 + to_integer(unsigned(offset)) * 32 downto 0 + 32 * to_integer(unsigned(offset)));
+               s_waitrequest <= '0';
+               state <= I;
+           else
+               --Miss
+               --if dirty, write cache block to mem then fetch block from memory
+               if (dirty = '1') then
+                   state <= MW;
+               else
+               --if clean, directly fetch from memory
+                   state <= MR;
+               end if;
+           end if;
+       else
+           --if invalid, then fetch block from memory
+           state <= MR;
+       end if;
+      when W =>
+      when MR =>
+      when MW =>
+      when MWAIT =>
+
+    end case;
+
+  end if;
+
+end process cache;
+
+
 
 end arch;
