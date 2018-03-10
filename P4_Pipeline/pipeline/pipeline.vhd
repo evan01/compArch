01de05790b4d708b265dd0a30e7c9c0feadfffd7
Callum May <callumdmay@gmail.com>
2018-03-09 22:13:02 -0500

    Add program counter, scaffolding for pipeline.vhd

--- P4_Pipeline/pipeline/pipeline.vhd
+++ P4_Pipeline/pipeline/pipeline.vhd
@@ -1,1 +1,15 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
+entity pipeline is
+ Port (
+   clk : in std_logic;
+   reset : in std_logic
+   );
+end pipeline;
+
+architecture arch of pipeline is
+begin
+
+end arch;
---
+++ P4_Pipeline/pipeline/program_counter.vhd
@@ -0,0 +1,25 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity program_counter is
+ Port (
+   clk : in std_logic;
+   reset : in std_logic;
+   input_address : in std_logic_vector (31 downto 0) := (others => '0');
+   output_address : out std_logic_vector(31 downto 0) := (others => '0')
+   );
+end program_counter;
+
+architecture arch of program_counter is
+begin
+
+pc: process(clk)
+begin
+  if (reset = '1') then
+    output_address <= (others => '0');
+  elsif (rising_edge(clk)) then
+    output_address <= input_address;
+  end if;
+end process;
+end arch;
