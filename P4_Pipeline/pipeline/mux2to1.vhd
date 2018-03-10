692dd736abb4d00fecc9acf42cf921153f326f2b
Callum May <callumdmay@gmail.com>
2018-03-10 10:50:10 -0500

    Add basic wiring for IF stage in pipeline

--- P4_Pipeline/pipeline/mux2to1.vhd
+++ P4_Pipeline/pipeline/mux2to1.vhd
@@ -2,13 +2,15 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 entity mux2to1 is
-    Port ( SEL : in  STD_LOGIC;
-           A   : in  STD_LOGIC_VECTOR (31 downto 0);
-           B   : in  STD_LOGIC_VECTOR (31 downto 0);
+    Port ( sel : in  STD_LOGIC;
+          --0 INPUT
+           input_0  : in  STD_LOGIC_VECTOR (31 downto 0);
+           --1 INPUT
+           input_1   : in  STD_LOGIC_VECTOR (31 downto 0);
            X   : out STD_LOGIC_VECTOR (31 downto 0));
 end mux2to1;
 
 architecture arch of mux2to1 is
 begin
-    X <= A when (SEL = '1') else B;
+    X <= input_0 when (sel = '0') else input_1;
 end arch;
--- P4_Pipeline/pipeline/pipeline.vhd
+++ P4_Pipeline/pipeline/pipeline.vhd
@@ -3,13 +3,66 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity pipeline is
- Port (
+ port (
    clk : in std_logic;
    reset : in std_logic
    );
 end pipeline;
 
 architecture arch of pipeline is
+
+------------------------- IF STAGE -------------------------
+component program_counter is
+ port (
+   clk : in std_logic;
+   reset : in std_logic;
+   input_address : in std_logic_vector (31 downto 0) := (others => '0');
+   output_address : out std_logic_vector(31 downto 0) := (others => '0')
+   );
+end component;
+
+component mux2to1 is
+    port ( sel : in  STD_LOGIC;
+           input_0  : in  STD_LOGIC_VECTOR (31 downto 0);
+           input_1   : in  STD_LOGIC_VECTOR (31 downto 0);
+           X   : out STD_LOGIC_VECTOR (31 downto 0));
+end component;
+
+component byte_adder is
+ port (
+   input_address : in std_logic_vector(31 downto 0);
+   output_address : out std_logic_vector(31 downto 0)
+ );
+end component;
+
+signal pc_output_address: std_logic_vector(31 downto 0);
+signal pc_input_address: std_logic_vector(31 downto 0);
+signal incremented_pc_address: std_logic_vector(31 downto 0);
+signal branch_address: std_logic_vector(31 downto 0);
+signal pc_sel: std_logic;
+------------------------- IF STAGE -------------------------
+
 begin
+
+------------------------- IF STAGE -------------------------
+  pc: program_counter PORT MAP(
+    clk => clk,
+    reset => reset,
+    input_address => pc_input_address,
+    output_address => pc_output_address
+  );
+
+  pc_input_mux : mux2to1 PORT MAP(
+    sel => pc_sel,
+    input_0 => incremented_pc_address,
+    input_1 => branch_address,
+    X => pc_input_address
+  );
+
+  pc_incrementer: byte_adder PORT MAP(
+    input_address => pc_output_address,
+    output_address => incremented_pc_address
+  );
+------------------------- IF STAGE -------------------------
 
 end arch;
