938e393cd5c4a84dab2671a89542abbc822c2012
Callum May <callumdmay@gmail.com>
2018-03-10 16:04:41 -0500

    Add IF ID register

--- P4_Pipeline/pipeline/byte_adder.vhd
+++ P4_Pipeline/pipeline/byte_adder.vhd
@@ -3,10 +3,10 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity byte_adder is
- Port (
-   input_address : in std_logic_vector(31 downto 0);
-   output_address : out std_logic_vector(31 downto 0)
- );
+port (
+  input_address : in std_logic_vector(31 downto 0);
+  output_address : out std_logic_vector(31 downto 0)
+);
 end byte_adder;
 
 architecture arch of byte_adder is
