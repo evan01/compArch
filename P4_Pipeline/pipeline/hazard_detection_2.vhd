ff70d96452ad61e8e6664785f5dc4a44474751bd
Evan Knox <eknoxmobile@gmail.com>
2018-03-28 13:54:16 -0400

    Hazzard detection 2 underway with tests!

--- P4_Pipeline/pipeline/hazard_detection_2.vhd
+++ P4_Pipeline/pipeline/hazard_detection_2.vhd
@@ -1,7 +1,9 @@
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
-ENTITY hazard_detection IS
+use work.hazzard_detection_pkg.all;
+
+ENTITY hazard_detection_2 IS
 	PORT(
 		clock				 : IN std_logic;
 		id_instruction : IN  std_logic_vector(31 DOWNTO 0); -- instruction in if-id stage
@@ -13,33 +15,17 @@ ENTITY hazard_detection IS
 		pc_write             : OUT std_logic := '1'; -- used to stall current instruction
 		fflush               : OUT std_logic := '0' -- Flush instructions if j type
 	);
-END hazard_detection;
+END hazard_detection_2;
 
+ARCHITECTURE arch OF hazard_detection_2 IS
 
-ARCHITECTURE arch OF hazard_detection IS
-
-	TYPE instr IS (
-		addi, slti, andi, ori, xori, lui, lw, sw,
-		s_ll, s_rl, s_ra, add, sub, mult, div, slt, nd, o_r, n_or, x_or, mfhi, mflo,
-		jr,jal,j,
-        beq, bne,
-        invalid
+    COMPONENT instruction_decoder IS
+	PORT(
+		id_instruction : in std_logic_vector(31 DOWNTO 0); -- instruction
+		instruction    :  out instr;
+		instruction_type: out instr_type
 	);
-
-    TYPE instr_type IS (
-        r_type, 
-        i_type, 
-        j_type, 
-        b_type
-    );
-    
-    TYPE hazzard_state is (
-        stall,
-        flush,
-        bubble,
-        normal,
-        start
-    );
+    END component;
 
     signal state: hazzard_state := start;
     signal next_state: hazzard_state := normal;
@@ -48,61 +34,18 @@ ARCHITECTURE arch OF hazard_detection IS
     signal last_instruction_type : instr_type;
 
 begin
-
-    instruction_type <=
-            i_type when       id_instruction(31 downto 26) = "001000" else
-            i_type when       id_instruction(31 downto 26) = "001000" else
-            i_type when       id_instruction(31 downto 26) = "001100" else
-            i_type when       id_instruction(31 downto 26) = "001101" else
-            i_type when       id_instruction(31 downto 26) = "001110" else
-            i_type when       id_instruction(31 downto 26) = "001111" else
-            j_type when       id_instruction(31 downto 26) = "000000" and id_instruction(5 downto 0) = "001000" else
-            i_type when       id_instruction(31 downto 26) = "100011" else
-            i_type when       id_instruction(31 downto 26) = "101011" else
-            b_type when       id_instruction(31 downto 26) = "000100" else
-            b_type when       id_instruction(31 downto 26) = "000101" else
-            j_type when       id_instruction(31 downto 26) = "000010" else
-            j_type when       id_instruction(31 downto 26) = "000011" else
-            r_type;
-
-    instruction <=
-        addi    when      id_instruction(31 downto 26) = "001000"    else
-        slti    when      id_instruction(31 downto 26) = "001010"    else
-        andi    when      id_instruction(31 downto 26) = "001100"    else
-        ori     when      id_instruction(31 downto 26) = "001101"    else
-        xori    when      id_instruction(31 downto 26) = "001110"    else
-        lui     when      id_instruction(31 downto 26) = "001111"    else
-        s_ll    when      id_instruction(31 downto 26) = "000000"    and id_instruction(5 downto 0) =  "000000" else
-        s_rl    when      id_instruction(31 downto 26) = "000000"    and id_instruction(5 downto 0) =  "000010" else
-        s_ra    when      id_instruction(31 downto 26) = "000000"    and id_instruction(5 downto 0) =  "000011" else
-        jr      when      id_instruction(31 downto 26) = "000000"    and id_instruction(5 downto 0) =  "001000" else
-        add     when      id_instruction(31 downto 26) = "000000"    and id_instruction(5 downto 0) =  "100000" else
-        sub     when      id_instruction(31 downto 26) = "000000"    and id_instruction(5 downto 0) =  "100010" else
-        mult    when      id_instruction(31 downto 26) = "000000"    and id_instruction(5 downto 0) =  "011000" else
-        div     when      id_instruction(31 downto 26) = "000000"    and id_instruction(5 downto 0) =  "011010" else
-        slt     when      id_instruction(31 downto 26) = "000000"    and id_instruction(5 downto 0) =  "101010" else
-        nd      when      id_instruction(31 downto 26) = "000000"    and id_instruction(5 downto 0) =  "100100" else
-        o_r     when      id_instruction(31 downto 26) = "000000"    and id_instruction(5 downto 0) =  "100101" else
-        n_or    when      id_instruction(31 downto 26) = "000000"    and id_instruction(5 downto 0) =  "100111" else
-        x_or    when      id_instruction(31 downto 26) = "000000"    and id_instruction(5 downto 0) =  "100110" else
-        mfhi    when      id_instruction(31 downto 26) = "000000"    and id_instruction(5 downto 0) =  "010000" else
-        mflo    when      id_instruction(31 downto 26) = "000000"    and id_instruction(5 downto 0) =  "010010" else
-        lw      when      id_instruction(31 downto 26) = "100011"    else
-        sw      when      id_instruction(31 downto 26) = "101011"    else
-        beq     when      id_instruction(31 downto 26) = "000100"    else
-        bne     when      id_instruction(31 downto 26) = "000101"    else
-        j       when      id_instruction(31 downto 26) = "000010"    else
-        jal     when      id_instruction(31 downto 26) = "000011"    else
-        invalid;
+    ----INSTANTIATE DECODER COMPONENT, for sanity sake, outputs instruction and type
+	hd : instruction_decoder 
+	port map(
+        id_instruction,
+        instruction,
+        instruction_type
+	);
 
     process(id_instruction, instruction, instruction_type) -- Does this handle the case where you get 2 of the same instruction in a row??? Use PC?
- 
     begin
         state <= bubble;
     end process;
-    
-    
-    
     
     --ONLY the outputs/state should be determined by the clock, and held for 1 period.
     process(state)
--- P4_Pipeline/pipeline/hazzard_detection_2_tb.vhd
+++ P4_Pipeline/pipeline/hazzard_detection_2_tb.vhd
@@ -1,43 +1,54 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
+use work.hazzard_detection_pkg.all;
+use work.all;
 
-entity hazzard_detection_tb is 
-end hazzard_detection_tb;
+entity hazzard_detection_tb2 is 
+end hazzard_detection_tb2;
 
-architecture arch of hazzard_detection_tb is 
+architecture arch of hazzard_detection_tb2 is 
 
-	component hazzard_detection is
+	component hazard_detection_2 is
 		port(
-			clock :std_logic;
-			id_instruction : std_logic_vector(31 DOWNTO 0); -- instructionif-id stage
-			ex_rd_register : std_logic_vector(4 DOWNTO 0); -- rt registerthe id-ex stage
-			ex_rs_register : std_logic_vector(4 DOWNTO 0); -- rt registerthe id-ex stage
-			ex_rt_register : std_logic_vector(4 DOWNTO 0); -- rt registerthe id-ex stage
-			idex_out_mem_read : std_logic; -- if memory is being read
-			mux_flush : std_logic := '0'; -- To add bubble
-			pc_write : std_logic := '1'; -- used to stall current instruction
-			fflush : std_logic := '0' -- Flush instructions if j type
+			clock				 : IN std_logic;
+			id_instruction : IN  std_logic_vector(31 DOWNTO 0); -- instruction in if-id stage
+			ex_rd_register : IN  std_logic_vector(4 DOWNTO 0); -- rt register in the id-ex stage
+			ex_rs_register : IN  std_logic_vector(4 DOWNTO 0); -- rt register in the id-ex stage
+			ex_rt_register : IN  std_logic_vector(4 DOWNTO 0); -- rt register in the id-ex stage
+			idex_out_mem_read    : IN  std_logic; -- if memory is being read
+			mux_flush            : OUT std_logic; -- To add bubble
+			pc_write             : OUT std_logic; -- used to stall current instruction
+			fflush               : OUT std_logic -- Flush instructions if j type
 		);
 	end component;
 
-	--EVAN MIDDLE OF WRITING TESTS FOR THIS THING... 
-	--insantiate signals with default start values
-	signal clk : std_logic := '0';
+	signal clock : std_logic := '0';
 	constant clk_period : time := 1 ns;
-	signal ex_rd_register,
-	signal ex_rs_register,
-	signal ex_rt_register,
-	signal idex_out_mem_read,
-	signal mux_flush,
-	signal pc_write,
-		fflush
+	signal id_instruction : std_logic_vector(31 downto 0);
+	signal ex_rd_register: std_logic_vector(4 DOWNTO 0);
+	signal ex_rs_register: std_logic_vector(4 DOWNTO 0);
+	signal ex_rt_register: std_logic_vector(4 DOWNTO 0);
+	signal idex_out_mem_read: std_logic;
+	signal mux_flush : std_logic;
+	signal pc_write : std_logic;
+	signal fflush : std_logic;
+	
+	type instr_array is array(5 downto 0) of std_logic_vector(31 downto 0);
+	signal instructions : instr_array := (
+		"00100000000000010000000000000001",
+		"00100000000000100000000000000010",
+		"00100000000000110000000000000011",
+		"00010000011000000000000000000010",
+		"00100000000000111111111111111111",
+		"00001000000000000000000000000011"
+	);
 
 begin
 	----INSTANTIATE COMPONENT
-	hd : hazzard_detection 
+	haz : hazard_detection_2 
 	port map(
-		clk,
+		clock,
 		id_instruction,
 		ex_rd_register,
 		ex_rs_register,
@@ -51,9 +62,9 @@ begin
 	--CLOCK
 	clk_process : process
 	begin
-		clk <= '0';
+		clock <= '0';
 		wait for clk_period/2;
-		clk <= '1';
+		clock <= '1';
 		wait for clk_period/2;
 	end process;
 	
@@ -63,16 +74,10 @@ begin
 	procedure test_hd_unit is 
 	begin
 	
+	id_instruction <= instructions(0);
 	wait for clk_period;
-
-	00100000000000010000000000000001
-	00100000000000100000000000000010
-	00100000000000110000000000000011
-	00010000011000000000000000000010
-	00100000000000111111111111111111
-	00001000000000000000000000000011
-
-			
+	id_instruction <= instructions(1);
+	
 	end procedure;
 
 	begin
