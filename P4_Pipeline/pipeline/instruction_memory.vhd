2f199556a7f8c0dfe759467b9b1e2500504e2505
Callum May <callumdmay@gmail.com>
2018-03-29 12:42:02 -0400

    Add working hazard detection

--- P4_Pipeline/pipeline/instruction_memory.vhd
+++ P4_Pipeline/pipeline/instruction_memory.vhd
@@ -6,7 +6,7 @@ use ieee.std_logic_textio.all;
 --NOTE, the majority of this code is taken from the Quartus Design and Synthesis handbook
 --as well as the faculty of Engineering at McGill University, Computer Architecture Course
 
-entity instruction_memory is 
+entity instruction_memory is
 	generic(
 		-- ram_size : INTEGER := 32768;
 		ram_size : integer := 8192; --This is in WORDS
@@ -15,7 +15,7 @@ entity instruction_memory is
 	port(
 		clock: in std_logic;
 		pc : in std_logic_vector (31 downto 0);
-		instruction_out: out std_logic_vector (31 downto 0) := (31 downto 6 => '0')&"100000"
+		instruction_out: out std_logic_vector (31 downto 0)
 	);
 end instruction_memory;
 
@@ -35,20 +35,23 @@ begin
 			for i in 0 to ram_size-1 loop
 				ram(i) <= std_logic_vector(to_unsigned(0,32));
 			end loop;
-			
+
 			file_open(file_pointer, "program.txt", READ_MODE);
 			while not endfile(file_pointer) loop
 				readline(file_pointer, line_num);
         		read(line_num, data);
 				ram(counter) <= data;
-				counter := counter + 1;
+        if (counter = 0) then
+          instruction_out <= data;
+        end if;
+        counter := counter + 1;
 			end loop;
 			file_close(file_pointer);
 			instructions_loaded <= '1';
 		end if;
 
 		--Memory logic
-		if(rising_edge(clock)) then
+		if(falling_edge(clock)) then
 			if(to_integer(unsigned(pc)) < 8192) then
 				instruction_out <= ram(to_integer(unsigned(pc))/4);
 			end if;
