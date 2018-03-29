7b7948bc530b4b0a0a9ba535a588924cd4b7750b
evan01 <eknoxmobile@gmail.com>
2018-03-29 16:36:32 -0400

    Fixed the assembly program to have lw in it

--- P4_Pipeline/assembler/sw_lw_br.asm
+++ P4_Pipeline/assembler/sw_lw_br.asm
@@ -2,7 +2,8 @@ Start:  addi $1, $0, 12
         addi $2, $0, 12
         sw $1,40($0)
         sw $2,44($0)
+        lw $3,40($0)
         beq $1, $2, Finish
 
-Finish: add $3, $2, $$1
+Finish: add $4, $2, $$1
         jr $31
