0983c93453d84ee9d57e4fc41ac73745804702f3
Evan Knox <eknoxmobile@gmail.com>
2018-04-06 18:51:20 -0400

    compiling branch predictor

--- P5_SplitCache/pipeline/branch_predictor.vhd
+++ P5_SplitCache/pipeline/branch_predictor.vhd
@@ -5,10 +5,10 @@ USE work.hazzard_detection_pkg.all;
 
 entity branch_predictor is
     generic (
-        history_table_size : integer := 32768;
+        history_table_size : integer := 32768
     );
     port (
-        clk: in : std_logic;
+        clk: in std_logic;
         if_instruction: in std_logic_vector(31 downto 0);
         id_branch_taken: in std_logic;
         id_branch_dest: in std_logic_vector(31 downto 0); --todo investigate why we need this.
@@ -31,15 +31,11 @@ architecture arch of branch_predictor is
     --Signals required for making predictions
     signal inst_type: instr_type;
     signal inst: instr;
-    variable bht_row: std_logic_vector(9 downto 0);
-    variable br_indx: std_logic_vector(15 downto 0);
-    variable br_history: std_logic_vector(1 downto 0);
-    variable br_predictor: std_logic_vector(1 downto 0);
+    
 
     --Signals required for updating the bht
     signal prediction_made: std_logic := '0';
-    variable update_indx: std_logic_vector(15 downto 0);
-    variable update_row: std_logic_vector(9 downto 0);
+    
 
     --Component definitions
     component instruction_decoder IS
@@ -48,7 +44,7 @@ architecture arch of branch_predictor is
             instruction    : OUT  instr;
             instruction_type: OUT instr_type
         );
-    END instruction_decoder;
+    END component;
 
 
 begin
@@ -59,7 +55,11 @@ begin
         inst_type
     );
 
-    clk_process: process(clk):
+    clk_process: process(clk)
+	variable bht_row: std_logic_vector(9 downto 0);
+    	variable br_indx: std_logic_vector(15 downto 0);
+    	variable br_history: std_logic_vector(1 downto 0);
+    	variable br_predictor: std_logic_vector(1 downto 0);
     begin
         if(rising_edge(clk)) then 
             if(inst_type = b_type) then
@@ -67,7 +67,7 @@ begin
                     --first get the branch target addr 
                     br_indx := if_instruction(15 downto 0);
                     --Then get the branch history table row
-                    bht_row := bht(to_integer(unsigned(br_indx));
+                    bht_row := bht(to_integer(unsigned(br_indx)));
                     --Then get the history
                     br_history := bht_row(9 downto 8);
                     --Based on the history, select the predictor
@@ -79,7 +79,9 @@ begin
                         when "10" =>
                             br_predictor := bht_row(5 downto 4);	
                         when "11" =>
-                            br_predictor := bht_row(7 downto 6);	
+                            br_predictor := bht_row(7 downto 6);
+			when others =>
+			    br_predictor := "XX";	
                     end case;
                     
                     --Based on the predictor, make a prediction
@@ -92,8 +94,10 @@ begin
                             predict_br_taken <= '1';
                         when "11" =>
                             predict_br_taken <= '1';
+			when others =>
+			    predict_br_taken <= 'X';
                     end case;
-                    br_dest : out std_logic_vector(31 downto 0);
+                    br_dest <= id_branch_dest;
                     prediction_made <= '1';
             end if;
 
@@ -102,14 +106,15 @@ begin
     end process;
     
     --Need to know exactly 1 cc after we made a prediction whether or not the prediction was correct.
-    update_prediction: process(prediction_made):
+    update_prediction: process(prediction_made)
+	variable bht_indx: integer;
     begin
          if(prediction_made = '1') then
                 --update bht
                     --first get the branch target addr indx
-                    bht_indx := id_branch_dest(15 downto 0);
+                    bht_indx := to_integer(unsigned(id_branch_dest(15 downto 0)));
                     --Then get the branch history table row and update it (shift history left and add newest result)
-                    bht(to_integer(unsigned(br_indx))(9 downto 8) <= bht(to_integer(unsigned(br_indx))(8) & id_branch_taken;
+                    bht(bht_indx) <= bht(bht_indx)(8) & id_branch_taken & bht(bht_indx)(7 downto 0);
                     prediction_made <= '0';
             end if;
     end process;
