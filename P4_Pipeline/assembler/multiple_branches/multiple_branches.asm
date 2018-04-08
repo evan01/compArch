2d1cd69711cb7f3f1ee1df000c8eb6ffe68a7f42
Callum May <callumdmay@gmail.com>
2018-04-08 16:30:56 -0400

    Commit changes to assembly files

--- P4_Pipeline/assembler/multiple_branches/multiple_branches.asm
+++ P4_Pipeline/assembler/multiple_branches/multiple_branches.asm
@@ -1,15 +1,15 @@
-addi    $1, $0, 10
+        addi    $1, $0, 10
         addi $2, $0, 1
         addi $3, $0, 0
         addi $4, $0, 2
-Start:  beq $1, $0, Finish
+Start:  beq $1, $0, Finish #PC 16
         sub $1, $1, $2
-        bne $3, $0, If2
+        bne $3, $0, If1 #PC 24
         addi $3, $0, 1
-        bne $3, $2, If2 
+If1:    bne $3, $2, If2 # PC 32
         addi $3, $0, 2
-        j Start      
-If2:    bne $3, $4, Start  
-        addi $3, $0, 0       
+        j Start
+If2:    bne $3, $4, Start #PC 44
+        addi $3, $0, 0
 If3:     j Start
-Finish: add $4, $2, $1
+Finish: add $4, $2, $1 # PC 56
